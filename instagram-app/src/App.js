820f81691e19abb76c9e31af738274a33359cb63
abidex4yemi <abidex4yemi@gmail.com>
2019-06-03 12:59:25 +0100

    Import data from data folder and set as initial app state

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { data } from './data/data';
+const initialState = data;
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">Instagram clone</header>
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = initialState;
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">Instagram clone</header>
+			</div>
+		);
+	}
 }
 
 export default App;
